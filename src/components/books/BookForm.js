7a5155b58527e57c4dd9dbeb6cc60e15a88ac582
Stephen Gambill <stephensgambill@gmail.com>
2023-03-18 22:13:58 -0500

    230318 Chapter additions to books create form

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -7,23 +7,77 @@ export const BookForm = () => {
 
     const [newBook, updateNewBook] = useState([])
     const navigate = useNavigate()
+    const [bookChapterTitleArray, updateBookChapterTitleArray] = useState([])
+    const [chapterInputs, setChapterInputs] = useState([])//for updating number of chapters
+    const [chapterTitles, setChapterTitles] = useState([])
 
     const localUser = localStorage.getItem("bookClub_member")
     const userObject = JSON.parse(localUser)
 
 
+    useEffect(() => {
+        const chapterInputs = [];
+        for (let i = 1; i <= bookChapterTitleArray.length; i++) {
+            chapterInputs.push(
+                <input
+                    key={`chapter-${i}`}
+                    type="text"
+                    className="form-control"
+                    placeholder={`Chapter ${i}`}
+                    onChange={(evt) => {
+                        const copy = [...bookChapterTitleArray]
+                        copy[i - 1] = evt.target.value
+                        updateBookChapterTitleArray(copy)
+                    }}
+                />
+            )
+        }
+        setChapterInputs(chapterInputs)
+    }, [bookChapterTitleArray])
 
-    const handleSaveButtonClick = (event) => {
-        event.preventDefault()
 
-        setNewBook(newBook)
-            .then(() => {
-                navigate("/bookList")
-            })
+    const handleChapterNumberChange = (evt) => {
+        const numChapters = parseInt(evt.target.value)
+        updateBookChapterTitleArray(new Array(numChapters).fill(""))
     }
 
 
 
+
+    const handleSaveButtonClick = (event) => {
+        event.preventDefault()
+        const chapters = []
+
+        console.log(bookChapterTitleArray)
+
+        const copy = { ...newBook }
+        copy.chapters = bookChapterTitleArray.length
+        updateNewBook(copy)
+
+        setNewBook(newBook)
+            .then((newBookArray) => {
+                for (let i = 0; i < bookChapterTitleArray.length; i++) {
+                    const chapterTitle = bookChapterTitleArray[i] || `Chapter ${i + 1}`
+                    const chapter = {
+                        order: i + 1,
+                        bookId: newBookArray.id,
+                        title: chapterTitle || (`Chapter ${i + 1}`)
+                    }
+                    fetch("http://localhost:8088/bookChapters", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(chapter),
+                    })
+
+                }
+            })
+
+        navigate("/bookList")
+
+
+    }
 
     return (<form
         className="bookForm">
@@ -75,14 +129,33 @@ export const BookForm = () => {
                         copy.image = evt.target.value
                         updateNewBook(copy)
                     }} />
+                <label htmlFor="chapterNumbers">How many chapters?</label>
+                <input
+                    required
+                    type="number"
+                    id="chapterNumbers"
+                    className="form-control"
+                    onChange={handleChapterNumberChange}
+                />
+
+                <label htmlFor="chapters">Chapter Titles</label>
+                {chapterInputs}
+
+
             </div>
             <button
                 className="btn btn-primary"
                 onClick={(evt) => {
-                    const copy = { ...newBook }
                     handleSaveButtonClick(evt)
                 }
                 }>Save Book
+            </button>
+            <button
+                className="btn btn-primary"
+                onClick={(evt) => {
+                    navigate("/bookList")
+                }
+                }>Cancel
             </button>
         </fieldset>
 
